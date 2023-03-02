973f3a20eeb558921297c5976a24c1c0b4cb1c3e
Jordan <rectorjordan94@gmail.com>
2023-03-02 12:47:13 -0600

    makes cat model in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    desription = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+# make migrations and migrate after creating models or by making any changes that are going to reflect in our database
