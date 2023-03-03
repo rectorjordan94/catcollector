17140e29618f9f91f751848caa07ba17a90eb9da
Jordan <rectorjordan94@gmail.com>
2023-03-03 15:45:10 -0600

    adds toys to cat detail and ability to add and remove from a cat

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 # import your models here
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 
 # Register your models here.
 admin.site.register(Cat)
 admin.site.register(Feeding)
+admin.site.register(Toy)
+
